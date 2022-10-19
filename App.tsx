e6b28545c287b6b9951369977b6445d3f50041d8
kledovskaya <k.ledovskaya@unwind.ru>
2022-10-19 23:56:56 +0300

    links change

--- App.tsx
+++ App.tsx
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {WebView} from 'react-native-webview';
 
+const links = [
+  'https://ok.ru/videoembed/13180506',
+  'https://medtaor.akamaized.net/v1/master/a0d007312bfd99c47f76b77ae26b1ccdaae76cb1/plives/140622/v6news/playlist.m3u8',
+  'https://medtaor.akamaized.net/v1/master/a0d007312bfd99c47f76b77ae26b1ccdaae76cb1/plives/110322/tv9telugu/playlist.m3u8',
+  'https://medtaor.akamaized.net/v1/master/a0d007312bfd99c47f76b77ae26b1ccdaae76cb1/plives/110322/channel24/playlist.m3u8',
+];
+
 const App = () => {
+  const [active, setActive] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setActive(state => (state < links.length - 1 ? state + 1 : 0));
+    }, 3 * 60 * 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <WebView
       javaScriptEnabled={true}
       scrollEnabled={false}
       allowsFullscreenVideo={true}
       source={{
-        uri: `https://ok.ru/videoembed/13180506`,
+        uri: links[active],
       }}
     />
   );
